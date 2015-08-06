cd40c2b72863f32a5cef2f8461ae78fb01e9c8be
Abhishek Dutt <abhishekdutt@outlook.com>
2015-08-06 21:06:28 +0530

    Init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sailsVoterFeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sailsVoterFeApp
+ */
+angular.module('sailsVoterFeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sailsVoterFeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sailsVoterFeApp
+ */
+angular.module('sailsVoterFeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
